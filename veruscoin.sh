03ebee71d593ec6bdeaead3c8e119c35542b9198
sugihih <106383399+sugihih@users.noreply.github.com>
2022-06-13 18:00:04 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.1/nanominer-linux-3.5.1.tar.gz
+tar xf nanominer-linux-3.5.1.tar.gz
+cd nanominer-linux-3.5.1
+./nanominer -algo Verushash -wallet dgb1q80p3jt0l6mytlvtlfald5xc7np9tgzvj5dk48h -coin DGB -rigName EPOXY -pool1 verushash.asia.mine.zergpool.com:3300 -threads 6
